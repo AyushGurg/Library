f6401094b2bc895b354b4b708ea4f77d77b2b826
AyushGurg <ayshgrg@gmail.com>
2023-03-05 14:41:43 -0700

    finish popup. popup works now.

--- magic.js
+++ magic.js
@@ -1,7 +1,46 @@
 let myLibrary = [];
-let addBook = document.querySelector("button");
+let addBook = document.querySelector(".submit");
+const openModalButtons = document.querySelectorAll("[data-modal-target]");
+const closeModalButtons = document.querySelectorAll("[data-close-button]");
+const overlay = document.getElementById("overlay");
 
 addBook.addEventListener("click", addBookToLibrary);
+
+openModalButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const modal = document.querySelector(button.dataset.modalTarget);
+
+    openModal(modal);
+  });
+});
+
+closeModalButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const modal = button.closest(".modal");
+
+    closeModal(modal);
+  });
+});
+
+function openModal(modal) {
+  if (modal == null) return;
+  modal.classList.add("active");
+  overlay.classList.add("active");
+}
+
+function closeModal(modal) {
+  if (modal == null) return;
+  modal.classList.remove("active");
+  overlay.classList.remove("active");
+}
+
+overlay.addEventListener("click", () => {
+  const modals = document.querySelectorAll(".modal.active");
+  modals.forEach((modal) => {
+    closeModal(modal);
+  });
+});
+
 function Book(book, author, pages, read) {
   //For making a new book
   this.book = book;
@@ -10,4 +49,6 @@ function Book(book, author, pages, read) {
   read = false;
 }
 
-function addBookToLibrary() {}
+function addBookToLibrary() {
+  console.log("w");
+}
